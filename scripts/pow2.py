4dc06103c777d54c357c11ca474e898700388368
Fernando Borretti <fernando@borretti.me>
2026-05-27 17:24:34 +1000

    style

--- scripts/pow2.py
+++ scripts/pow2.py
@@ -7,11 +7,13 @@ for n in range(2, 17):
         print("---")
         print()
     print(f"Q: $2^{{{n}}}$")
+    print()
     print(f"A: ${2**n}$")
     print()
     print("---")
     print()
     print(f"Q: $\\log_2 {2**n}$")
+    print()
     print(f"A: ${n}$")
     if n < 16:
         print()
