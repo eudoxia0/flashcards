0f718cfabf96588117aba81911f818ce1114a97c
Fernando Borretti <fernando@borretti.me>
2026-07-13 06:39:44 +0800

    add tmpl.py script to generate flashcards from a CSV and a template

---
+++ scripts/tmpl.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+Generate flashcards from a CSV and a template.
+"""
+
+import argparse
+import csv
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Generate flashcards from a CSV and template."
+    )
+    parser.add_argument("--csv", required=True, help="Path to the CSV file.")
+    parser.add_argument("--template", required=True, help="Path to the template file.")
+    parser.add_argument(
+        "--output", required=True, help="Path to the output Markdown file."
+    )
+    parser.add_argument("--title", required=True, help="Deck title.")
+    args = parser.parse_args()
+    title: str = args.title
+
+    with open(args.template, "r") as f:
+        template = f.read()
+
+    with open(args.csv, newline="") as f:
+        rows = list(csv.DictReader(f))
+
+    blocks = [template.format(**row) for row in rows]
+
+    with open(args.output, "w") as f:
+        f.write("---\n")
+        f.write(f"title = '{title}'\n")
+        f.write("---\n\n")
+        f.write("\n\n---\n\n".join(blocks))
+        f.write("\n")
+
+
+if __name__ == "__main__":
+    main()
