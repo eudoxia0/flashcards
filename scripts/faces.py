6796bc5d7103c650bb4d7250ea7941e319fe4f69
Fernando Borretti <fernando@borretti.me>
2026-07-20 11:23:49 +1000

    simplify

--- scripts/faces.py
+++ scripts/faces.py
@@ -12,11 +12,7 @@ def main():
     images: list[tuple[str, str]] = []
     for path in DIR.iterdir():
         filename: str = path.name
-        name: str
-        if "—" in filename:
-            name = filename.split("—")[0]
-        else:
-            name = path.stem
+        name: str = path.stem
         images.append((filename, name))
 
     # Sort by name.
